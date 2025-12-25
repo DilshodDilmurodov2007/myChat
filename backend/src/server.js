de5d62fb18a14d0cb2cfdc7e1d83694a89b711f4
Dan Best <dilshoddilmurodov799@gmail.com>
2025-12-26 01:47:43 +0500

    frontend & backend merged in the same port

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
+import path from 'path'
 
 
 dotenv.config()
@@ -9,10 +10,23 @@ dotenv.config()
 const PORT = process.env.PORT || 3000;
 
 const app = express()
+const __dirname = path.resolve()
 
 app.use("/api/auth", authRoutes)    
 app.use("/api/messages", messageRoutes)    
 
-app.listen(PORT, () => console.log("Server running in Port "+ PORT))
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(
+        express.static(
+            path.join(__dirname, "../frontend/dist")
+        )
+    )
+    app.get("", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
+
+app.listen(PORT, () => console.log("Server running on port "+ PORT))
 
 
